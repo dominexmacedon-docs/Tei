2bc098beb917e13cb58c08e77937750327d93860
Dominex Macedon <dominexmacedon@gmail.com>
2026-06-19 19:52:48 +0630

    Update vm.h

---
+++ src/vm/vm.h
@@ -0,0 +1,38 @@
+#ifndef VM_H
+#define VM_H
+
+#include "chunk.h"
+#include "stack.h"
+#include "native.h"
+
+typedef enum {
+    VM_OK,
+    VM_RUNTIME_ERROR
+} VMResult;
+
+typedef struct {
+    Chunk *chunk;
+    uint8_t *ip;
+
+    Stack stack;
+
+    NativeRegistry natives;
+} VM;
+
+void vm_init(VM *vm);
+
+void vm_free(VM *vm);
+
+void vm_reset_stack(VM *vm);
+
+void vm_push(VM *vm, Value value);
+
+Value vm_pop(VM *vm);
+
+Value vm_peek(VM *vm, int distance);
+
+VMResult vm_run(VM *vm);
+
+VMResult vm_interpret(VM *vm, Chunk *chunk);
+
+#endif
