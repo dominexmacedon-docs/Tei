c84fb8cc196330af4ef4e9854707c49a5cf92aa7
Dominex Macedon <dominexmacedon@gmail.com>
2026-06-20 10:05:10 +0630

    Update test_vm.c

--- tests/test_vm.c
+++ tests/test_vm.c
@@ -1,109 +1,132 @@
 #include <stdio.h>
-
-#include "../src/vm/chunk.h"
-#include "../src/vm/opcode.h"
-#include "../src/vm/value.h"
 #include "../src/vm/vm.h"
+#include "../src/vm/opcode.h"
+#include "../src/vm/chunk.h"
+#include "../src/vm/value.h"
 
-static void emit_op(Chunk *chunk, OpCode op) {
-    chunk_write(chunk, (unsigned char)op, 0);
+static void write_op(Chunk *chunk, OpCode op, int line) {
+    chunk_write(chunk, (unsigned char)op, line);
 }
 
-static void emit_constant(Chunk *chunk, Value value) {
+static void write_constant(Chunk *chunk, Value value, int line) {
     int index = chunk_add_constant(chunk, value);
-
-    emit_op(chunk, OP_PUSH);
-    chunk_write(chunk, (unsigned char)index, 0);
+    chunk_write(chunk, (unsigned char)index, line);
 }
 
-int main(void) {
+static void test_arithmetic() {
+    printf("Running arithmetic test...\n");
+
     Chunk chunk;
     chunk_init(&chunk);
 
-    printf("========== BUILDING BYTECODE ==========\n");
+    write_op(&chunk, OP_PUSH, 1);
+    write_constant(&chunk, value_number(10), 1);
 
-    /* 10 + 20 = 30 */
-    emit_constant(&chunk, value_number(10));
-    emit_constant(&chunk, value_number(20));
-    emit_op(&chunk, OP_ADD);
-    emit_op(&chunk, OP_SHOW);
+    write_op(&chunk, OP_PUSH, 1);
+    write_constant(&chunk, value_number(20), 1);
 
-    /* 100 - 58 = 42 */
-    emit_constant(&chunk, value_number(100));
-    emit_constant(&chunk, value_number(58));
-    emit_op(&chunk, OP_SUB);
-    emit_op(&chunk, OP_SHOW);
-
-    /* 6 * 7 = 42 */
-    emit_constant(&chunk, value_number(6));
-    emit_constant(&chunk, value_number(7));
-    emit_op(&chunk, OP_MUL);
-    emit_op(&chunk, OP_SHOW);
-
-    /* 100 / 5 = 20 */
-    emit_constant(&chunk, value_number(100));
-    emit_constant(&chunk, value_number(5));
-    emit_op(&chunk, OP_DIV);
-    emit_op(&chunk, OP_SHOW);
-
-    /* 22 % 5 = 2 */
-    emit_constant(&chunk, value_number(22));
-    emit_constant(&chunk, value_number(5));
-    emit_op(&chunk, OP_MOD);
-    emit_op(&chunk, OP_SHOW);
-
-    /* -8 */
-    emit_constant(&chunk, value_number(8));
-    emit_op(&chunk, OP_NEG);
-    emit_op(&chunk, OP_SHOW);
-
-    /* 10 == 10 */
-    emit_constant(&chunk, value_number(10));
-    emit_constant(&chunk, value_number(10));
-    emit_op(&chunk, OP_EQUAL);
-    emit_op(&chunk, OP_SHOW);
-
-    /* 10 != 20 */
-    emit_constant(&chunk, value_number(10));
-    emit_constant(&chunk, value_number(20));
-    emit_op(&chunk, OP_NOT_EQUAL);
-    emit_op(&chunk, OP_SHOW);
-
-    /* 20 > 10 */
-    emit_constant(&chunk, value_number(20));
-    emit_constant(&chunk, value_number(10));
-    emit_op(&chunk, OP_GREATER);
-    emit_op(&chunk, OP_SHOW);
-
-    /* 5 < 10 */
-    emit_constant(&chunk, value_number(5));
-    emit_constant(&chunk, value_number(10));
-    emit_op(&chunk, OP_LESS);
-    emit_op(&chunk, OP_SHOW);
-
-    /* !false */
-    emit_constant(&chunk, value_bool(0));
-    emit_op(&chunk, OP_NOT);
-    emit_op(&chunk, OP_SHOW);
-
-    /* push then pop */
-    emit_constant(&chunk, value_number(999));
-    emit_op(&chunk, OP_POP);
-
-    emit_op(&chunk, OP_HALT);
+    write_op(&chunk, OP_ADD, 1);
+    write_op(&chunk, OP_SHOW, 1);
+    write_op(&chunk, OP_HALT, 1);
 
     VM vm;
     vm_init(&vm);
 
-    printf("========== RUNNING VM ==========\n");
-
-    VMResult result = vm_interpret(&vm, &chunk);
-
-    printf("================================\n");
-    printf("VM Result: %s\n", result == VM_OK ? "OK" : "ERROR");
+    vm_interpret(&vm, &chunk);
 
     vm_free(&vm);
     chunk_free(&chunk);
 
+    printf("Arithmetic test done.\n\n");
+}
+
+static void test_comparison() {
+    printf("Running comparison test...\n");
+
+    Chunk chunk;
+    chunk_init(&chunk);
+
+    write_op(&chunk, OP_PUSH, 1);
+    write_constant(&chunk, value_number(5), 1);
+
+    write_op(&chunk, OP_PUSH, 1);
+    write_constant(&chunk, value_number(10), 1);
+
+    write_op(&chunk, OP_LESS, 1);
+    write_op(&chunk, OP_SHOW, 1);
+    write_op(&chunk, OP_HALT, 1);
+
+    VM vm;
+    vm_init(&vm);
+
+    vm_interpret(&vm, &chunk);
+
+    vm_free(&vm);
+    chunk_free(&chunk);
+
+    printf("Comparison test done.\n\n");
+}
+
+static void test_jump() {
+    printf("Running jump test...\n");
+
+    Chunk chunk;
+    chunk_init(&chunk);
+
+    write_op(&chunk, OP_PUSH, 1);
+    write_constant(&chunk, value_number(1), 1);
+
+    write_op(&chunk, OP_JUMP_IF_FALSE, 1);
+    chunk_write(&chunk, 2, 1);
+    chunk_write(&chunk, 0, 1);
+
+    write_op(&chunk, OP_PUSH, 1);
+    write_constant(&chunk, value_number(999), 1);
+
+    write_op(&chunk, OP_SHOW, 1);
+    write_op(&chunk, OP_HALT, 1);
+
+    VM vm;
+    vm_init(&vm);
+
+    vm_interpret(&vm, &chunk);
+
+    vm_free(&vm);
+    chunk_free(&chunk);
+
+    printf("Jump test done.\n\n");
+}
+
+static void test_boolean() {
+    printf("Running boolean test...\n");
+
+    Chunk chunk;
+    chunk_init(&chunk);
+
+    write_op(&chunk, OP_PUSH, 1);
+    write_constant(&chunk, value_bool(1), 1);
+
+    write_op(&chunk, OP_NOT, 1);
+    write_op(&chunk, OP_SHOW, 1);
+    write_op(&chunk, OP_HALT, 1);
+
+    VM vm;
+    vm_init(&vm);
+
+    vm_interpret(&vm, &chunk);
+
+    vm_free(&vm);
+    chunk_free(&chunk);
+
+    printf("Boolean test done.\n\n");
+}
+
+int main() {
+    test_arithmetic();
+    test_comparison();
+    test_jump();
+    test_boolean();
+
+    printf("All VM tests completed.\n");
     return 0;
 }
