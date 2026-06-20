5afb42e6520cfa299f3b4f7a6da92ee8a69b5852
Dominex Macedon <dominexmacedon@gmail.com>
2026-06-20 10:01:03 +0630

    Create frame.c

---
+++ src/vm/frame.c
@@ -0,0 +1,46 @@
+#include "frame.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static void runtime_error(const char *msg) {
+    fprintf(stderr, "[Frame Error] %s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+void frame_init(CallStack *cs) {
+    cs->count = 0;
+}
+
+void frame_free(CallStack *cs) {
+    cs->count = 0;
+}
+
+CallFrame *frame_current(CallStack *cs) {
+    if (cs->count == 0) {
+        runtime_error("No active call frame");
+    }
+    return &cs->frames[cs->count - 1];
+}
+
+int frame_push(CallStack *cs, Chunk *chunk, uint8_t *ip, int stack_offset) {
+    if (cs->count >= FRAMES_MAX) {
+        runtime_error("Stack overflow: too many call frames");
+        return 0;
+    }
+
+    CallFrame *frame = &cs->frames[cs->count++];
+
+    frame->chunk = chunk;
+    frame->ip = ip;
+    frame->stack_offset = stack_offset;
+
+    return 1;
+}
+
+void frame_pop(CallStack *cs) {
+    if (cs->count == 0) {
+        runtime_error("Frame underflow");
+    }
+
+    cs->count--;
+}
