a5fed39f2ed4edd09b8ffeef708ca8278e4787f0
Dominex Macedon <dominexmacedon@gmail.com>
2026-06-20 06:44:16 +0630

    Update test_vm.c

--- tests/test_vm.c
+++ tests/test_vm.c
@@ -6,109 +6,89 @@
 #include "../src/vm/vm.h"
 
 static void emit_op(Chunk *chunk, OpCode op) {
-    chunk_write(chunk, (unsigned char)op, 1);
+    chunk_write(chunk, (unsigned char)op, 0);
 }
 
 static void emit_constant(Chunk *chunk, Value value) {
     int index = chunk_add_constant(chunk, value);
 
     emit_op(chunk, OP_PUSH);
-    chunk_write(chunk, (unsigned char)index, 1);
-}
-
-static void emit_jump_if_false(Chunk *chunk, uint16_t offset) {
-    emit_op(chunk, OP_JUMP);
-    chunk->code[chunk->count - 1] = OP_JUMP_IF_FALSE;
-
-    chunk_write(chunk, (offset >> 8) & 0xff, 1);
-    chunk_write(chunk, offset & 0xff, 1);
-}
-
-static void emit_jump(Chunk *chunk, uint16_t offset) {
-    emit_op(chunk, OP_JUMP);
-
-    chunk_write(chunk, (offset >> 8) & 0xff, 1);
-    chunk_write(chunk, offset & 0xff, 1);
+    chunk_write(chunk, (unsigned char)index, 0);
 }
 
 int main(void) {
-
     Chunk chunk;
     chunk_init(&chunk);
 
     printf("========== BUILDING BYTECODE ==========\n");
 
-    /*
-        show 10 + 20;
-    */
-
+    /* 10 + 20 = 30 */
     emit_constant(&chunk, value_number(10));
     emit_constant(&chunk, value_number(20));
     emit_op(&chunk, OP_ADD);
     emit_op(&chunk, OP_SHOW);
 
-    /*
-        if (1 < 2)
-            show 111;
-    */
+    /* 100 - 58 = 42 */
+    emit_constant(&chunk, value_number(100));
+    emit_constant(&chunk, value_number(58));
+    emit_op(&chunk, OP_SUB);
+    emit_op(&chunk, OP_SHOW);
 
-    emit_constant(&chunk, value_number(1));
-    emit_constant(&chunk, value_number(2));
+    /* 6 * 7 = 42 */
+    emit_constant(&chunk, value_number(6));
+    emit_constant(&chunk, value_number(7));
+    emit_op(&chunk, OP_MUL);
+    emit_op(&chunk, OP_SHOW);
+
+    /* 100 / 5 = 20 */
+    emit_constant(&chunk, value_number(100));
+    emit_constant(&chunk, value_number(5));
+    emit_op(&chunk, OP_DIV);
+    emit_op(&chunk, OP_SHOW);
+
+    /* 22 % 5 = 2 */
+    emit_constant(&chunk, value_number(22));
+    emit_constant(&chunk, value_number(5));
+    emit_op(&chunk, OP_MOD);
+    emit_op(&chunk, OP_SHOW);
+
+    /* -8 */
+    emit_constant(&chunk, value_number(8));
+    emit_op(&chunk, OP_NEG);
+    emit_op(&chunk, OP_SHOW);
+
+    /* 10 == 10 */
+    emit_constant(&chunk, value_number(10));
+    emit_constant(&chunk, value_number(10));
+    emit_op(&chunk, OP_EQUAL);
+    emit_op(&chunk, OP_SHOW);
+
+    /* 10 != 20 */
+    emit_constant(&chunk, value_number(10));
+    emit_constant(&chunk, value_number(20));
+    emit_op(&chunk, OP_NOT_EQUAL);
+    emit_op(&chunk, OP_SHOW);
+
+    /* 20 > 10 */
+    emit_constant(&chunk, value_number(20));
+    emit_constant(&chunk, value_number(10));
+    emit_op(&chunk, OP_GREATER);
+    emit_op(&chunk, OP_SHOW);
+
+    /* 5 < 10 */
+    emit_constant(&chunk, value_number(5));
+    emit_constant(&chunk, value_number(10));
     emit_op(&chunk, OP_LESS);
-
-    /*
-        Skip THEN block if false.
-
-        THEN block =
-            PUSH 111
-            SHOW
-
-        Size = 4 bytes
-    */
-
-    emit_jump_if_false(&chunk, 4);
-
-    emit_constant(&chunk, value_number(111));
     emit_op(&chunk, OP_SHOW);
 
-    /*
-        if (2 < 1)
-            show 999;
-        else
-            show 222;
-    */
-
-    emit_constant(&chunk, value_number(2));
-    emit_constant(&chunk, value_number(1));
-    emit_op(&chunk, OP_LESS);
-
-    /*
-        THEN size = 4 bytes
-        JUMP size = 3 bytes
-        total skip = 7
-    */
-
-    emit_jump_if_false(&chunk, 7);
-
-    emit_constant(&chunk, value_number(999));
-    emit_op(&chunk, OP_SHOW);
-
-    /*
-        Skip ELSE
-    */
-
-    emit_jump(&chunk, 4);
-
-    emit_constant(&chunk, value_number(222));
-    emit_op(&chunk, OP_SHOW);
-
-    /*
-        show !(0);
-    */
-
-    emit_constant(&chunk, value_number(0));
+    /* !false */
+    emit_constant(&chunk, value_bool(0));
     emit_op(&chunk, OP_NOT);
     emit_op(&chunk, OP_SHOW);
+
+    /* push then pop */
+    emit_constant(&chunk, value_number(999));
+    emit_op(&chunk, OP_POP);
 
     emit_op(&chunk, OP_HALT);
 
