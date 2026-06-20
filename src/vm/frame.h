375336b7932de2dc6f8a73c7b7205020a9ab23d6
Dominex Macedon <dominexmacedon@gmail.com>
2026-06-20 10:00:13 +0630

    Create frame.h

---
+++ src/vm/frame.h
@@ -0,0 +1,29 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+#include <stdint.h>
+#include "chunk.h"
+#include "value.h"
+
+#define FRAMES_MAX 64
+
+typedef struct {
+    Chunk *chunk;      
+    uint8_t *ip;       
+    int stack_offset; 
+} CallFrame;
+
+typedef struct {
+    CallFrame frames[FRAMES_MAX];
+    int count;
+} CallStack;
+
+void frame_init(CallStack *cs);
+void frame_free(CallStack *cs);
+
+CallFrame *frame_current(CallStack *cs);
+
+int frame_push(CallStack *cs, Chunk *chunk, uint8_t *ip, int stack_offset);
+void frame_pop(CallStack *cs);
+
+#endif
