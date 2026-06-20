8550f2ded3bc081c755ebe5fe55f9bd09a0ad722
Dominex Macedon <dominexmacedon@gmail.com>
2026-06-20 10:12:21 +0630

    Update vm.h

--- src/vm/vm.h
+++ src/vm/vm.h
@@ -1,9 +1,11 @@
 #ifndef VM_H
 #define VM_H
+
 #include <stdint.h>
 #include "chunk.h"
 #include "stack.h"
 #include "native.h"
+#include "frame.h"
 
 typedef enum {
     VM_OK,
@@ -15,24 +17,20 @@ typedef struct {
     uint8_t *ip;
 
     Stack stack;
-
     NativeRegistry natives;
+    CallStack frames;
 } VM;
 
 void vm_init(VM *vm);
-
 void vm_free(VM *vm);
 
 void vm_reset_stack(VM *vm);
 
 void vm_push(VM *vm, Value value);
-
 Value vm_pop(VM *vm);
-
 Value vm_peek(VM *vm, int distance);
 
 VMResult vm_run(VM *vm);
-
 VMResult vm_interpret(VM *vm, Chunk *chunk);
 
 #endif
