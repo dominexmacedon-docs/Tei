9c1a8f17448d99ec8883d7f4770e5687c842c859
Dominex Macedon <dominexmacedon@gmail.com>
2026-06-20 11:32:53 +0630

    Update value.h

--- src/vm/value.h
+++ src/vm/value.h
@@ -3,11 +3,14 @@
 
 #include <stddef.h>
 
+typedef struct Object Object;
+
 typedef enum {
     VALUE_NULL,
     VALUE_BOOL,
     VALUE_NUMBER,
-    VALUE_STRING
+    VALUE_STRING,
+    VALUE_OBJECT
 } ValueType;
 
 typedef struct {
@@ -17,6 +20,7 @@ typedef struct {
         int boolean;
         double number;
         char *string;
+        Object *object;
     } as;
 } Value;
 
@@ -24,6 +28,7 @@ Value value_null(void);
 Value value_bool(int value);
 Value value_number(double value);
 Value value_string(const char *value);
+Value value_object(Object *object);
 
 void value_free(Value *value);
 Value value_copy(Value value);
